2c39cd3bb266711b631fe59eab8bc20dbe240d44
Bas <basarab.o@icloud.com>
2026-02-27 13:59:40 +0200

    add validation

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -6,13 +6,15 @@ import {
   deleteNote,
   updateNote,
 } from '../controllers/notesController.js';
+import { noteIdSchema } from '../validations/notesValidation.js';
+import { celebrate } from 'celebrate';
 
 const router = Router();
 
 router.get('/notes', getAllNotes);
-router.get('/notes/:noteId', getNoteById);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 router.patch('/notes/:noteId', updateNote);
 
 export default router;
