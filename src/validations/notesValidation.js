4bcb23be1a703a7ee436809007ed0fa6880a357d
Bas <basarab.o@icloud.com>
2026-02-27 20:24:55 +0200

    edit updateNote

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -71,9 +71,9 @@ export const updateNoteSchema = {
     }),
     tag: Joi.string()
       .valid(...TAGS)
-      .min(1)
       .messages({
         'any.only': `Tag must be one of: ${TAGS}`,
-      }),
+      })
+      .min(1),
   }),
 };
